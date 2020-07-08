28740775b3e709bfd8601688de489285a57770c9
Tom Corbin <30200976+ThwCorbin@users.noreply.github.com>
2020-07-08 17:48:47 -0400

    Add array variables, CSS colors, and bases and counter event listeners

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 console.log("hiya");
+
+// * Variables ********************
+let turtles = [];
+let bases = [];
+let colors = [
+	"--red",
+	"--orange",
+	"	--yellow",
+	"--green",
+	"--indigo",
+	"--violet",
+];
+let boardBase = document.querySelector(".board-base");
+let counter = document.querySelector(".move-counter");
+
+// * Classes ********************
+// * Functions ********************
+let changeCount = () => console.log("This is a counter");
+// * ********************
+// * ********************
+// * ********************
+// * ********************
+// * ********************
+// * ********************
+// * Event listeners ********************
+counter.addEventListener("click", changeCount);
+boardBase.addEventListener("click", (e) => {
+	console.log(e.target);
+});
+
+// * Invoke on load ********************
