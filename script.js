207e35ec124b8cf0206425f533e09ceec316c340
Tom Corbin <30200976+ThwCorbin@users.noreply.github.com>
2020-07-10 10:25:36 -0400

    Add messages, refactor from game.illegalMove to game.displayMessage method, add directions

--- script.js
+++ script.js
@@ -18,8 +18,15 @@ class Game {
 		this.turtleInPlay = false;
 		this.turtleInDOM = "";
 	}
-	illegalMove() {
-		console.log("Illegal move");
+	displayMessage(msg, playAgain) {
+		message.textContent = msg;
+		window.setTimeout(function () {
+			if (playAgain) {
+				message.textContent = `Hit reset to play again`;
+				return;
+			}
+			message.textContent = `Restack the turtles on base two or three to win`;
+		}, 3000);
 	}
 }
 
@@ -45,9 +52,9 @@ class Base {
 
 checkForWin = () => {
 	if (basesArr[1].turtles.length === 7 || basesArr[2].turtles.length === 7) {
-		console.log("Winner !!!!");
-		// todo: winner message & reset button to play agains
 		game.gameActive = false;
+		game.displayMessage("Player Wins!!!", true);
+		// todo: reset button to play agains
 	} else {
 		return;
 	}
@@ -63,6 +70,7 @@ let moveTurtle = (e) => {
 	// * if no turtle has been selected, return
 	if (!game.turtleInPlay) {
 		// todo: add message to pick a turtle to move first
+		game.displayMessage(`Pick a turtle first`);
 		return;
 		// * if the base is the same (coercion) as the selected turtle
 	} else if (column == turtlesArr[game.idxTurtle].gridColumn) {
@@ -142,12 +150,14 @@ let selectTurtle = (e) => {
 		// * if this turtle is not a top turtle
 	} else if (!turtlesArr[idx].topTurtle) {
 		// * ignore clicks on a turtle that is not on top of a stack
-		console.log("not top turtle");
+		game.displayMessage(
+			`That turtle is not on top of its stack; select a different turtle`
+		);
 		return;
 		// * if there is already another turtle selected
 	} else if (game.turtleInPlay) {
-		// todo: ignore or let player know that fact
-		console.log("Another turtle has already been selected");
+		game.displayMessage(`Another turtle has already been selected`);
+		return;
 	} else {
 		// * highlight turtle border
 		turtle.style.border = `.25em solid var(--highlight)`;
@@ -165,7 +175,9 @@ let setUpBoard = () => {
 		string += `<div class="turtle turtle-${i}"}></div>`;
 	}
 	turtleGrid.innerHTML = string;
-
+	game.displayMessage(
+		`Ready to play!!  Select the top turtle in the stack to begin!`
+	);
 	game.gameActive = true;
 };
 
@@ -210,38 +222,3 @@ let turtle1 = document.querySelector(".turtle-1");
 let moveThisTurtle = () => {
 	turtle1.style.grid["grid-column"] = "2/3";
 };
-// * Obsolete code...probably ********************
-
-// let transX = idx === 0 ? idx : idx * 30;
-// let transY =
-// 	turtlesOnBase === 0
-// 		? 2 * turtleObj.yMultiple
-// 		: 2 * (turtleObj.yMultiple - turtlesOnBase);
-// game.turtleInDOM.style.transform = `translate(${transX}vw, ${transY}em)`;
-
-// let yStart = 6;
-// this.yMultiple = yStart--;
-
-// let colors = [
-// 	"--red",
-// 	"--orange",
-// 	"--yellow",
-// 	"--green",
-// 	"--blue",
-// 	"--indigo",
-// 	"--violet",
-// ];
-
-// let sizes = ["16", "18", "20", "22", "24", "26", "28"];
-// let sizes = ["28", "26", "24", "22", "20", "18", "16"];
-// let turtleStartPosition = 6;
-
-// this.color = colors[0];
-// this.size = sizes[0];
-// this.position = turtleStartPosition;
-// this.topTurtle = this.size === "16" ? true : false;
-
-// // Prepare for next iteration
-// colors.shift();
-// sizes.shift();
-// turtleStartPosition--;
